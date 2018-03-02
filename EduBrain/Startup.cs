4507da7c11919305ae9304de24e204aa881f530a
Rafael Berger <GeorgeJohnson24790@gmail.com>
2018-03-02 12:37:13 -0500

    Initial

---
+++ EduBrain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EduBrain.Startup))]
+namespace EduBrain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
