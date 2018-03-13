fe33fb3f4a9b55879f0399f44f26af281cbf6c5c
Rafael Berger <GeorgeJohnson24790@gmail.com>
2018-03-13 16:40:31 -0400

    Added Itext For pdf

--- EduBrain/Controllers/EmailController.cs
+++ EduBrain/Controllers/EmailController.cs
@@ -6,11 +6,22 @@ using System.Web.Http.Results;
 using System.Web.Mvc;
 using System.Net;
 using System.Net.Mail;
+using EduBrain.Models;
+using EduBrain.ViewModels;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+//using iTextSharp.text.pdf;
+//using iTextSharp.text;
+
+//using iTextSharp.text.pdf;
 
 namespace EduBrain.Controllers
 {
     public class EmailController : Controller
     {
+
+        private EduSmart_dbEntities _db = new EduSmart_dbEntities();
         // GET: Email
         public ActionResult Index()
         {
@@ -18,12 +29,38 @@ namespace EduBrain.Controllers
         }
 
     [HttpPost]
-    public ActionResult Email(string model)
+    public ActionResult Email(AcceptanceLetterVm acceptanceLetterVm)
     {
+            if (ModelState.IsValid)
+            {
+                // do your stuff like: save to database and redirect to required page.
+                Student student = new Student();
+                Person person = new Person();
 
-            MailMessage mm = new MailMessage("RafiBerger613@gmail.com", "aberger@ytehouston.org");
+           
+
+
+            }
+
+            // If we got this far, something failed, redisplay form
+
+            var writer = new PdfWriter(@"C:\Users\Rafib\OneDrive\Documents/hello_world.pdf");
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+            document.Add(new Paragraph("Hello World!"));
+            document.Close();
+
+            MailMessage mm = new MailMessage("RafiBerger613@gmail.com", "RafiBerger613@gmail.com");
             mm.Subject = "EduBrainz Email sender";
-            mm.Body = "This was sent from the eduBrainz app. Who would you like the sender of the email to be (what emailAddress)?";
+        
+
+
+           
+            mm.Body = "Dear Mr " + acceptanceLetterVm.LastName + 
+                " Congratulations on the acceptance of your  son, " + acceptanceLetterVm.ChildFirstName + " into Grade " + acceptanceLetterVm.Grade;
+
+
+
             mm.IsBodyHtml = false;
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
@@ -32,6 +69,7 @@ namespace EduBrain.Controllers
             NetworkCredential nc = new NetworkCredential("Rafiberger613@gmail.com", "Statistics24790");
             smtp.UseDefaultCredentials = true;
             smtp.Credentials = nc;
+            mm.Attachments.Add(new Attachment(@"C:\Users\Rafib\OneDrive\Documents\hello_world.pdf"));
             smtp.Send(mm);
             ViewBag.Message = "mail Has been sent";
 
---
+++ EduBrain/ViewModels/AcceptanceLetterVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduBrain.ViewModels
+{
+    public class AcceptanceLetterVm
+    {
+        public string FatherFirstName { get; set; }
+        public string MotherName { get; set; }
+        public string LastName { get; set; }
+        public string  ChildFirstName { get; set; }
+        public string Grade { get; set; }
+        [Display(Name = "Grade Entering")]
+        public int SelectedGradeId { get; set; }
+        public IEnumerable<SelectListItem> Grades { get; set; }
+
+
+
+
+    }
+}
