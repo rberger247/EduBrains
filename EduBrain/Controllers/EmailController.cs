7fb7bef235f4b5022f88878ce4b8f35afc458666
Rafael Berger <GeorgeJohnson24790@gmail.com>
2018-03-14 12:09:23 -0400

    Added Email with Pdf Attachment functionality

--- EduBrain/Controllers/EmailController.cs
+++ EduBrain/Controllers/EmailController.cs
@@ -8,9 +8,12 @@ using System.Net;
 using System.Net.Mail;
 using EduBrain.Models;
 using EduBrain.ViewModels;
-using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
+//using iText.Kernel.Pdf;
+//using iText.Layout;
+//using iText.Layout.Element;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 //using iTextSharp.text.pdf;
 //using iTextSharp.text;
 
@@ -35,46 +38,79 @@ namespace EduBrain.Controllers
             {
                 // do your stuff like: save to database and redirect to required page.
                 Student student = new Student();
-                Person person = new Person();
-
-           
-
-
+                Person person = new Person();        
             }
 
-            // If we got this far, something failed, redisplay form
+            var doc = new Document();
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(doc, memoryStream);
 
-            var writer = new PdfWriter(@"C:\Users\Rafib\OneDrive\Documents/hello_world.pdf");
-            var pdf = new PdfDocument(writer);
-            var document = new Document(pdf);
-            document.Add(new Paragraph("Hello World!"));
-            document.Close();
+            doc.Open();
+            doc.Add(new Paragraph("Dear Mr " + acceptanceLetterVm.LastName + "," +
+                    " Congratulations on the acceptance of your son, " + acceptanceLetterVm.ChildFirstName + " into the " + acceptanceLetterVm.Grade + " grade" +
+                    " we are excited to have " + acceptanceLetterVm.ChildFirstName + " in our school"));
 
-            MailMessage mm = new MailMessage("RafiBerger613@gmail.com", "RafiBerger613@gmail.com");
-            mm.Subject = "EduBrainz Email sender";
-        
+            writer.CloseStream = false;
+            doc.Close();
+            memoryStream.Position = 0;
 
+            MailMessage mm = new MailMessage("RafiBerger613@gmail.com", acceptanceLetterVm.EmailAdress)
+            {
+                Subject = " Congratulations on your childs acceptance into school",
+                IsBodyHtml = false,
+                Body = "Dear Mr " + acceptanceLetterVm.LastName + "," + 
+                    " Congratulations on the acceptance of your son, " + acceptanceLetterVm.ChildFirstName + " into the " + acceptanceLetterVm.Grade + " grade" +
+                    " we are excited to have " + acceptanceLetterVm.ChildFirstName +  " in our school"
+            };
 
-           
-            mm.Body = "Dear Mr " + acceptanceLetterVm.LastName + 
-                " Congratulations on the acceptance of your  son, " + acceptanceLetterVm.ChildFirstName + " into Grade " + acceptanceLetterVm.Grade;
+            mm.Attachments.Add(new Attachment(memoryStream, "AcceptanceLetter.pdf"));
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                Credentials = new NetworkCredential("Rafiberger613@gmail.com", "")
 
+            };
 
-
-            mm.IsBodyHtml = false;
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 587;
-            smtp.EnableSsl = true;
-            NetworkCredential nc = new NetworkCredential("Rafiberger613@gmail.com", "Statistics24790");
-            smtp.UseDefaultCredentials = true;
-            smtp.Credentials = nc;
-            mm.Attachments.Add(new Attachment(@"C:\Users\Rafib\OneDrive\Documents\hello_world.pdf"));
             smtp.Send(mm);
-            ViewBag.Message = "mail Has been sent";
 
+
+            //MailMessage message = new MailMessage("RafiBerger613@gmail.com", "RafiBerger613@gmail.com");
+            //    message.Subject = "EduBrainz Email sender";
+
+
+
+
+            //    message.Body = ;
+
+
+
+            //message.IsBodyHtml = false;
+            //SmtpClient smtp = new SmtpClient();
+            //smtp.Host = "smtp.gmail.com";
+            //smtp.Port = 587;
+            //smtp.EnableSsl = true;
+            //NetworkCredential nc = new NetworkCredential("Rafiberger613@gmail.com", "Statistics24790");
+            //smtp.UseDefaultCredentials = true;
+            //smtp.Credentials = nc;
+            //message.Attachments.Add(new Attachment(output.ToString()));
+            //smtp.Send(message);
+
+            //doc.Close();
 
             return View();
+        }
+            // If we got this far, something failed, redisplay form
+
+            //    var writer = new PdfWriter(@"C:\Users\Rafib\OneDrive\Documents/hello_world.pdf");
+            //var pdf = new PdfDocument(writer);
+            //var document = new Document(pdf);
+            //document.Add(new Paragraph("Hello World!"));
+            //document.Close();
+
+            //ViewBag.Message = "mail Has been sent";
+
+
     }
     }
-}
--- EduBrain/ViewModels/AcceptanceLetterVm.cs
+++ EduBrain/ViewModels/AcceptanceLetterVm.cs
@@ -9,9 +9,18 @@ namespace EduBrain.ViewModels
 {
     public class AcceptanceLetterVm
     {
+        [Display(Name = "Father's First Name")]
         public string FatherFirstName { get; set; }
+        [Display(Name = "Father's Title")]
+        public string   FatherTitle { get; set; }
+
+        [Display(Name = "Email")]
+        public string EmailAdress { get; set; }
+
         public string MotherName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Child First Name")]
         public string  ChildFirstName { get; set; }
         public string Grade { get; set; }
         [Display(Name = "Grade Entering")]
